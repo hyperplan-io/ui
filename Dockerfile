e3d68306b7c552743797ae4ef8e5fc1660a88628
Antoine Sauray <sauray.antoine@gmail.com>
2019-06-29 20:23:17 +0200

    fix docker image

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM mhart/alpine-node:11 AS builder
+FROM mhart/alpine-node:10.16.0 AS builder
 WORKDIR /app
 COPY . .
+ARG SERVER="http:\/\/localhost:8090"
+RUN sed -i "s/process\.env\.REACT\_APP\_API\_ROOT/\"${SERVER}\"/g" /app/src/utils/Api.js
 RUN npm rebuild node-sass
+RUN yarn
 RUN yarn run build
 
 FROM mhart/alpine-node
 RUN yarn global add serve
-WORKDIR /app
 COPY --from=builder /app/build .
 CMD ["serve", "-p", "80", "-s", "."]
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '@blueprintjs/core/lib/css/blueprint.css';
+import { apiRootUrl } from './utils/Api';
 require('dotenv').config();
 
 const root = <App />;
 ReactDOM.render(root, document.getElementById('root'));
+console.log(`server root is ${apiRootUrl}`);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
